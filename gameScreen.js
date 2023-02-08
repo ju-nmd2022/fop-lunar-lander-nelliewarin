31aee191655d693bed827099502252c41e2a525c
nelliewarin <wane22bh@student.ju.se>
2023-02-08 12:57:08 +0100

    game

--- gameScreen.js
+++ gameScreen.js
@@ -1,23 +1,32 @@
-background(25, 52, 65 );
+background(25, 52, 65);
 noStroke();
 
 // ground
 fill(253, 234, 201);
-rect(0, 530, 700, 200);
+rect(0, 530, width, 200);
 
 // landing goal
-triangle(245, 510, 210, 530, 245, 530);
-triangle(435, 510, 435, 530, 465, 530);
-push();
-fill(190, 175, 151);
-ellipse(width/2, 522, 210, 60);
-pop();
-ellipse(width/2, 520, 200, 50);
+function landingGoal(x, y) {
+  push();
+  translate(x, y);
+  fill(253, 234, 201);
+  triangle(245, 510, 210, 530, 245, 530);
+  triangle(435, 510, 435, 530, 465, 530);
+  push();
+  fill(190, 175, 151);
+  ellipse(340, 522, 210, 60);
+  pop();
+  ellipse(340, 520, 200, 50);
+  pop();
+}
 
 //flag
 stroke(0);
 strokeWeight(5);
 line(50, 500, 50, 600);
+
 noStroke();
 fill(255, 0, 0);
 triangle(50, 500, 110, 515, 50, 550);
+
+landingGoal(0, 0);
--- rocketship.js
+++ rocketship.js
@@ -83,4 +83,4 @@ function rocketship(x, y, s) {
   pop();
 }
 
-rocketship(100, 100, 1);
+rocketship(50, 50, 1);
