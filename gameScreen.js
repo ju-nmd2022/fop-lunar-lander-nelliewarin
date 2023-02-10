322f1e9514cf2cfb12e46834291990404deb727c
nelliewarin <wane22bh@student.ju.se>
2023-02-10 11:28:18 +0100

    smaller landing goal

--- gameScreen.js
+++ gameScreen.js
@@ -1,3 +1,103 @@
+// landing goal
+function landingGoal(x, y, s) {
+  push();
+  scale(s);
+  translate(x, y);
+  fill(254, 140, 49);
+  triangle(245, 510, 210, 530, 245, 530);
+  triangle(435, 510, 435, 530, 465, 530);
+  fill(191, 105, 37);
+  ellipse(340, 522, 210, 60);
+  fill(255, 212, 178);
+  ellipse(340, 520, 200, 50);
+  pop();
+}
+
+// rocket ship
+function rocketship(x, y, s) {
+  push();
+  noStroke();
+  translate(x, y);
+  scale(s);
+  // Wings
+
+  push();
+  translate(250, 365);
+  rotate(2.1);
+  fill(159, 0, 0);
+  arc(-1, 10, 310, 180, 0, PI);
+  fill(255, 0, 0);
+  arc(0, 0, 310, 180, 0, PI);
+  pop();
+
+  push();
+  translate(250, 365);
+  rotate(-2.1);
+  fill(159, 0, 0);
+  arc(1, 10, 310, 180, 0, PI);
+  fill(255, 0, 0);
+  arc(0, 0, 310, 180, 0, PI);
+  pop();
+
+  //Bottom of rocket
+  fill(255, 0, 0);
+  triangle(201, 465, 250, 350, 299, 465);
+  fill(159, 0, 0);
+  triangle(205, 455, 250, 350, 295, 455);
+
+  // Body of rocketship
+  push();
+  fill(245, 245, 245);
+  translate(250, 450);
+
+  beginShape();
+  vertex(-50, 0);
+  bezierVertex(-100, -100, -100, -220, -50, -300);
+  line(-50, -300, 50, -300);
+  vertex(50, -300);
+  bezierVertex(100, -220, 100, -100, 50, 0);
+  line(50, 0, -50, 0);
+  endShape();
+
+  pop();
+  beginShape();
+  fill(255, 0, 0);
+  vertex(200, 150);
+  bezierVertex(245, 80, 250, 80, 300, 150);
+  endShape();
+
+  push();
+  translate(250, 450);
+  beginShape();
+  stroke(200, 200, 200, 100);
+  strokeWeight(15);
+  noFill();
+  vertex(0, -345);
+  bezierVertex(100, -270, 100, -100, 45, -8);
+  endShape();
+  pop();
+
+  // Window
+  fill(210, 210, 210);
+  ellipse(253, 255, 90);
+  fill(255, 0, 0);
+  ellipse(250, 250, 90);
+  fill(183, 233, 245);
+  ellipse(250, 250, 70);
+  fill(255);
+  ellipse(247, 247, 50);
+  fill(183, 233, 245);
+  ellipse(250, 250, 52);
+
+  //Middle wing
+  fill(159, 0, 0);
+  ellipse(253, 430, 26, 170);
+  fill(255, 0, 0);
+  ellipse(250, 430, 20, 170);
+
+  pop();
+}
+
 let starX = [];
 let starY = [];
 let starAlpha = [];
@@ -12,6 +112,7 @@ for (let i = 0; i < 500; i++) {
   starAlpha.push(alpha);
 }
 
+// Callinging evrything :)
 function draw() {
   background(25, 52, 65);
 
@@ -35,7 +136,6 @@ function draw() {
     ellipse(350, 670, 1100, 190);
     fill(255, 226, 203);
     ellipse(350, 680, 1100, 190);
-   
 
     //flag
     stroke(0);
@@ -47,21 +147,7 @@ function draw() {
     triangle(50, 500, 110, 515, 50, 550);
     noStroke();
 
-    landingGoal(0, 0, 1);
+    landingGoal(250, 350, 0.6);
+    rocketship(50, 50, 0.3);
   }
-}
-
-// landing goal
-function landingGoal(x, y, s) {
-  push();
-  scale(s);
-  translate(x, y);
-  fill(254, 140, 49);
-  triangle(245, 510, 210, 530, 245, 530);
-  triangle(435, 510, 435, 530, 465, 530);
-  fill(191, 105, 37);
-  ellipse(340, 522, 210, 60);
-  fill(255, 212, 178);
-  ellipse(340, 520, 200, 50);
-  pop();
 }
